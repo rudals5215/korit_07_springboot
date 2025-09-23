1de16c056c66efbf47a9bf253d4bac0187afcee2
rudals5215 <rudals5215@naver.com>
2025-09-23 09:28:22 +0900

    fear : 20250903 start

--- cardatabase/src/main/java/com/example/cardatabase/domain/OwnerRepository.java
+++ cardatabase/src/main/java/com/example/cardatabase/domain/OwnerRepository.java
@@ -1,4 +1,6 @@
 package com.example.cardatabase.domain;
 
-public interface Owner {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OwnerRepository extends JpaRepository<Owner, Long> {
 }
