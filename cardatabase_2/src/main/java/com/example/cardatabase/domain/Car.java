9492254aa0e128288ab6ed5873b506638bf2fbb9
rudals5215 <rudals5215@naver.com>
2025-09-24 14:36:48 +0900

    test

---
+++ cardatabase_2/src/main/java/com/example/cardatabase/domain/Car.java
@@ -0,0 +1,40 @@
+package com.example.cardatabase.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String brand, model, color, registrationNumber;
+    private int modelYear, price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner")
+    private Owner owner;
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public Car(String brand, String model, String color, String registrationNumber, int modelYear, int price, Owner owner) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.registrationNumber = registrationNumber;
+        this.modelYear = modelYear;
+        this.price = price;
+        this.owner = owner;
+    }
+}
