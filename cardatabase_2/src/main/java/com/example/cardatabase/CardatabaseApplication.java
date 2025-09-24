c8510eabfedc2c35f64c272c9416de68e208e68c
rudals5215 <rudals5215@naver.com>
2025-09-24 17:47:26 +0900

    feat : 20250924 RESTful API / Swagger / Spring Security / JWT

--- cardatabase_2/src/main/java/com/example/cardatabase/CardatabaseApplication.java
+++ cardatabase_2/src/main/java/com/example/cardatabase/CardatabaseApplication.java
@@ -1,9 +1,6 @@
 package com.example.cardatabase;
 
-import com.example.cardatabase.domain.Car;
-import com.example.cardatabase.domain.CarRepository;
-import com.example.cardatabase.domain.Owner;
-import com.example.cardatabase.domain.OwnerRepository;
+import com.example.cardatabase.domain.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -22,11 +19,14 @@ public class CardatabaseApplication implements CommandLineRunner {
 	// 여기에 생성자 주입 부분 적겠습니다.(그리고 .md파일로 옮기는 것도 함께 하겠습니다.)
 	private final CarRepository repository;
 	private final OwnerRepository ownerRepository;
+	private final AppUserRepository userRepository;
 
-    public CardatabaseApplication(CarRepository repository, OwnerRepository ownerRepository) {
+    public CardatabaseApplication(CarRepository repository, OwnerRepository ownerRepository, AppUserRepository userRepository) {
         this.repository = repository;
-		this.ownerRepository = ownerRepository;
+        this.ownerRepository = ownerRepository;
+        this.userRepository = userRepository;
     }
+
 
     public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
@@ -55,6 +55,12 @@ public class CardatabaseApplication implements CommandLineRunner {
 			logger.info("brand : {}, model : {}", car.getBrand(), car.getModel());
 		}
 
+		// AppUser 더미 데이터를 추가
+		// 저 위에 보시면 Owner의 경우에는 owner1 / owner2 만들어가지고 ownerRepository에 저장했었습니다.
+		// username: user / password : user
+		userRepository.save(new AppUser("user", "$2a$12$iaX.GGwrdDua.fmovSFix.F.eiAq915arr7jVgeGLyNyladzZ2nCK", "USER"));
+		// username:admin / password : admin
+		userRepository.save(new AppUser("admin","$2a$12$xjHCmhpb2z2AYRmxFxe7weR1jme0nG.M8pQ0mZs0N1LZJdmXcjQlO","ADMIN"));
 
 	}
 }
